f066b6935ad9ccfa55d6033fc0bd4663340c9421
Garrett Vanhoy <gvanhoy@itsfiziks-MacBook.local>
2018-04-07 18:30:23 -0700

    first commit

---
+++ src/SocialNetworkAnalysis.cpp
@@ -0,0 +1,24 @@
+//----------------------------------------------------------------------//
+// Author:
+// Net ID:
+// Date:
+//
+//----------------------------------------------------------------------//
+
+#include "SocialNetworkAnalysis.h"
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+
+SocialNetworkAnalysis::SocialNetworkAnalysis(string inputFilePath, string outputFilePath){
+    _inputFilePath = inputFilePath;
+    _outputFilePath = outputFilePath;
+}
+
+void SocialNetworkAnalysis::Run(){
+
+}
